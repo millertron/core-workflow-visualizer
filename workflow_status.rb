b54ec3e7c948366d3f7f7524b5e61c4aedc2ac2e
Millertron <senjinkoha@gmail.com>
2019-02-22 16:35:42 +0000

    Rubocop fix

--- workflow_status.rb
+++ workflow_status.rb
@@ -1,8 +1,11 @@
+# Individual Workflow status node to be rendered
 class WorkflowStatus
-	attr_reader :name, :available_actions
+  attr_reader :name, :available_actions
 
-	def initialize(name)
-		@name = name
-		@available_actions = Hash.new
-	end
+  def initialize(name)
+    @name = name
+    @available_actions = {}
+  end
+
+
 end
